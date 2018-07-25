e65330342afd9ab72536166ada08bff8715427c2
spartners <mariano.scandizzo@spartners.me>
2018-07-25 10:33:39 -0400

    version6

--- RandomGraph.py
+++ RandomGraph.py
@@ -1,20 +1,44 @@
 import string 
-from random import choice, randint
+from random import randint, sample
 from collections import defaultdict
 
 class RandomGraph:
     def __init__(self):
         self.letters = string.ascii_uppercase
-        self.adjacent_list = defaultdict(list)
-
+        self.adjacent_list = defaultdict()
+        self.nodos = list()
+        self.edge_list = list()
+        
 class GraphCreation(RandomGraph):
     def __init__(self,nodes):
         self.nodes = nodes
         super().__init__()
+           
+    def generate_edges(self):
+        self.nodos = sample(self.letters,self.nodes)
+        
+        for key in self.nodos:
+            noditos = self.nodos.copy()
+            noditos.remove(key)
+            self.adjacent_list[key] = sample(noditos,randint(0,len(noditos)))
+            
+        return self.adjacent_list
     
-    def generated_graph(self):
-        nodos = list()
-        for nod in range(self.nodes):
-            nod = choice(self.letters)
-            nodos.append(nod)
-        return nodos
+    def adjacency_matrix(self):
+        graph = self.generate_edges()
+
+        matrix_elements = sorted(graph.keys())
+        cols = rows = len(matrix_elements)
+
+        adjacency_matrix = [[0 for _ in range(rows)] for _ in range(cols)]
+
+        for key in matrix_elements:
+            for neighbor in graph[key]:
+                self.edge_list.append((key,neighbor))
+        
+        for edge in self.edge_list:
+            index_of_first_vertex = matrix_elements.index(edge[0])
+            index_of_second_vertex = matrix_elements.index(edge[1])
+            adjacency_matrix[index_of_first_vertex][index_of_second_vertex] = 1
+
+        return matrix_elements, self.edge_list, adjacency_matrix
